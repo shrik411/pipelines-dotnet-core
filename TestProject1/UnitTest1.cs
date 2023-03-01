05d15df98bafb211ee57b1989d1d61c3afaa6627
Shrikant Kamble <shrikant.kamble@necsws.com>
2023-03-01 17:41:56 +0530

    Added unit tests.

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace TestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            bool result = true; 
+
+            Assert.True(result);
+        }
+    }
+}
