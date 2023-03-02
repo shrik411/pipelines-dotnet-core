a1690384e2fa78703a6a47937cfeb4661d63675e
Shrikant Kamble <shrikant.kamble@necsws.com>
2023-03-02 12:49:38 +0530

    Unit tests.

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -5,7 +5,12 @@ namespace TestProject1
         [Fact]
         public void Test1()
         {
+            Assert.Equal(4, Add(2, 2));
+        }
 
+        int Add(int x, int y)
+        {
+            return x + y;
         }
     }
 }
